9ebdf5bb490da6710b24e93c98a8d4dc3bc40205
Sarina <sarina.condon@gmail.com>
2025-02-03 17:46:53 -0500

    made a few edits to receiver and transmitter

--- src/lora/receiver.cpp
+++ src/lora/receiver.cpp
@@ -1,20 +1,24 @@
 /*License: CC 4.0 - Attribution, NonCommercial (by Mitch Davis, github.com/thekakester)
 * https://creativecommons.org/licenses/by-nc/4.0/   (See README for details)*/
 #include <LoraSx1262.h>
-LoraSx1262 receiver;
+LoraSx1262* receiver;
 byte receiveBuff[255];
 void setup() {
   Serial.begin(9600);
   Serial.println("Booted");
 
-  if (!receiver.begin()) { //Initialize the radio
-    Serial.println("Failed to initialize radio");
-  }
+  // if (!receiver.begin()) { //Initialize the radio
+  //   Serial.println("Failed to initialize radio");
+  //   while (1);
+  // }
+  receiver = new LoraSx1262();
 }
 void loop() {
   //Receive a packet over radio
-  int bytesRead = receiver.lora_receive_async(receiveBuff, sizeof(receiveBuff));
+  Serial.print("Receiving... ");
+  int bytesRead = receiver->lora_receive_async(receiveBuff, sizeof(receiveBuff));
   if (bytesRead > -1) {
+    Serial.print("Received: ");
     Serial.write(receiveBuff, bytesRead);
     Serial.println();
   }
--- src/lora/transmitter.cpp
+++ src/lora/transmitter.cpp
@@ -1,20 +1,27 @@
 /*License: CC 4.0 - Attribution, NonCommercial (by Mitch Davis, github.com/thekakester)
 * https://creativecommons.org/licenses/by-nc/4.0/   (See README for details)*/
 #include <LoraSx1262.h>
-byte payload[255];
-LoraSx1262 radio;
+byte payload = "Hello World!";
+LoraSx1262* radio;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Booted");
 
-  if (!radio.begin()) { //Initialize radio
-    Serial.println("Failed to initialize radio.");
-  }
+  // if (!radio.begin()) { //Initialize radio
+  //   Serial.println("Failed to initialize radio.");
+  // }
+  radio = new LoraSx1262();
+
+  // data in payload
+  // for (int i = 0; i < 255; i++) {
+  //   payload[i] = i;
+  // }
 }
 void loop() {
+  const char *message = "Hello World!";
   Serial.print("Transmitting... ");
-  radio.transmit(payload, 255);
+  radio->transmit(payload, strlen(payload));
   Serial.println("Done!");
   delay(1000);
 }
